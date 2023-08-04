83bad0897d4047956889fad306ff9f9735446236
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2023-08-04 16:12:41 -0700

    Update main.tf

--- modules/service-account-iam-roles/main.tf
+++ modules/service-account-iam-roles/main.tf
@@ -1,30 +1,4 @@
-data "terraform_remote_state" "eks" {
-  backend = "s3"
-  config = {
-    bucket         = var.bucket
-    key            = format("env:/%s/%s/%s", var.workspace, "eks", "terraform.tfstate")
-    region         = var.bucket_region
-    dynamodb_table = var.dynamodb
-  }
-}
 
-data "terraform_remote_state" "dynamodb" {
-  backend = "s3"
-  config = {
-    bucket         = var.bucket
-    key            = format("env:/%s/%s/%s", var.workspace, "dynamodb", "terraform.tfstate")
-    region         = var.bucket_region
-    dynamodb_table = var.dynamodb
-  }
-}
-
-data "aws_eks_cluster" "cluster" {
-  name = data.terraform_remote_state.eks.outputs.cluster_id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = data.terraform_remote_state.eks.outputs.cluster_id
-}
 
 module "external_dns_role" {
   source = "git::https://github.com/dieple/terraform-framework.git//modules/iam-users-and-accounts/eks-sa-iam-role?ref=tags/v0.0.7"
