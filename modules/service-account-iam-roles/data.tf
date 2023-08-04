b906750f1378184a8fc06b2c18da1dd8ec612194
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2023-08-04 16:12:57 -0700

    Update data.tf

--- modules/service-account-iam-roles/data.tf
+++ modules/service-account-iam-roles/data.tf
@@ -1,3 +1,32 @@
+
+data "terraform_remote_state" "eks" {
+  backend = "s3"
+  config = {
+    bucket         = var.bucket
+    key            = format("env:/%s/%s/%s", var.workspace, "eks", "terraform.tfstate")
+    region         = var.bucket_region
+    dynamodb_table = var.dynamodb
+  }
+}
+
+data "terraform_remote_state" "dynamodb" {
+  backend = "s3"
+  config = {
+    bucket         = var.bucket
+    key            = format("env:/%s/%s/%s", var.workspace, "dynamodb", "terraform.tfstate")
+    region         = var.bucket_region
+    dynamodb_table = var.dynamodb
+  }
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_id
+}
+
 data "aws_availability_zones" "available" {}
 
 data "aws_caller_identity" "current" {}
